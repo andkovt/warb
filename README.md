d6f9a2834ad176ed13b8dd55cbbb079c38b4a192
Andrejs Kovtuns <andkovt@gmail.com>
2023-03-05 21:57:20 +0200

    Added readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Watch and Reload Browser
+
+Small application for monitoring file changes and the reloading active browser tab.
+
+## Setup
+- Install Chrome extension - https://github.com/andkovt/warb-extension
+- Download latest release from release tab - https://github.com/andkovt/warb/releases
+
+## Usage
+
+1. Launch Warb
+```bash
+./Warb ${DIRECTORY_TO_WATCH}
+```
+
+2. Enable Warb extension
--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+readonly VERSION="0.1"
 readonly TARGETS=(osx.13-arm64 osx-x64 linux-x64 linux-arm64 win-x64 win-arm64)
 readonly PUBLISH_PATH="Warb/bin/Release/net7.0"
 readonly RELEASE_PATH="release"
@@ -12,11 +13,12 @@ mkdir -p $RELEASE_PATH
 
 working_dir=$(pwd)
 cd $RELEASE_PATH
+rm -rf *
 full_release_path=$(pwd)
 cd ..
 
 for t in "${TARGETS[@]}"; do
   cd "${PUBLISH_PATH}/${t}/publish"
-  zip -r "$full_release_path/${t}.zip" .
+  zip -r "$full_release_path/${VERSION}-${t}.zip" .
   cd $working_dir
 done;
